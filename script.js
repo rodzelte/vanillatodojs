d9e767c6651d60c2de956a879691ccc4ae24dce0
rodzelte <zythonix.te@gmail.com>
2025-03-13 21:16:22 +0800

    try

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const submit = document.querySelector("#submit");
+const input = document.querySelector("#input");
+
+submit.addEventListener("click", () => {
+  const value = input.value;
+  if (value) {
+    console.log(value);
+    input.value = "";
+  } else {
+    const error = document.createElement("span");
+    error.textContent = "Please enter a value";
+    error.style.color = "red";
+    setInterval(submit.appendChild(error), 5000);
+    console.log("Please enter a value");
+  }
+});
