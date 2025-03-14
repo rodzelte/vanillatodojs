839f7f3ebe56283cc743a326119df3f838b50da0
rodzelte <zythonix.te@gmail.com>
2025-03-14 12:50:10 +0800

    finish

--- script.js
+++ script.js
@@ -2,39 +2,59 @@ const submit = document.querySelector("#submit");
 const input = document.querySelector("#input");
 const list = document.querySelector("#list");
 
-const todo = [];
+let todo = JSON.parse(localStorage.getItem("todo")) || []; 
+
+document.addEventListener("DOMContentLoaded", renderList); 
 
 submit.addEventListener("click", () => {
-  const value = input.value;
+  const value = input.value.trim();
   if (value) {
     todo.push(value);
-    console.log(todo);
+    localStorage.setItem("todo", JSON.stringify(todo)); 
     input.value = "";
-    list.innerHTML = "";
-    todo.forEach((item) => {
-      const li = document.createElement("li");
-      const deleteButton = document.createElement("button");
-      deleteButton.textContent = "Delete";
-
-      li.textContent = item;
-      list.appendChild(li);
-      list.appendChild(deleteButton);
-    });
+    renderList();
   } else {
-    if (!value) {
-      const error = document.createElement("span");
-      error.textContent = "Please enter a value";
-      error.style.color = "red";
-      error.classList.add("error-message");
-      submit.appendChild(error);
-
-      submit.disabled = true;
-
-      setTimeout(() => {
-        error.remove();
-        submit.disabled = false;
-      }, 1000);
-    }
-    console.log("Please enter a value");
+    showErrorMessage();
   }
 });
+
+function renderList() {
+  list.innerHTML = ""; 
+
+  todo.forEach((item, index) => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    li.style.display = "flex";  
+    li.style.alignItems = "center"; 
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.style.cursor = "pointer";
+    deleteButton.style.marginLeft = "10px";
+    deleteButton.style.backgroundColor = "red";
+
+    deleteButton.addEventListener("click", () => {
+      todo.splice(index, 1); 
+      localStorage.setItem("todo", JSON.stringify(todo)); 
+      renderList(); 
+    });
+
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+  });
+}
+
+function showErrorMessage() {
+  const error = document.createElement("span");
+  error.textContent = "Please enter a value";
+  error.style.color = "red";
+  error.classList.add("error-message");
+
+  submit.appendChild(error);
+  submit.disabled = true;
+
+  setTimeout(() => {
+    error.remove();
+    submit.disabled = false;
+  }, 1000);
+}
